fd8373bc69799b292c65fb54025c06299fb230bd
박성준 <myrhymetree@gmail.com>
2023-10-11 15:56:17 +0900

    init

---
+++ src/main/java/com/vsquad/iroas/IroasApplication.java
@@ -0,0 +1,13 @@
+package com.vsquad.iroas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IroasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IroasApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vsquad/iroas/IroasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vsquad.iroas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IroasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
