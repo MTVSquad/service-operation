aab7e46dbcb21c728efff2d6a54e552bcbc73561
박성준 <myrhymetree@gmail.com>
2023-10-21 00:01:58 +0900

    bugFix : fix security setting

--- src/main/java/com/vsquad/iroas/config/SecurityConfig.java
+++ src/main/java/com/vsquad/iroas/config/SecurityConfig.java
@@ -37,85 +37,27 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomUserDetailService userDetailsService;
 
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        // form Login을 disable하고, httpBasic을 disable하고, csrf를 disable하고, sessionManagement를 stateless로 설정
-//
-//        http
-//            .cors()
-//                .disable()
-//            .sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api/v1.0/**")
-//                .permitAll()
-//                .anyRequest()
-//                .authenticated()
-//            .and()
-//            .csrf()
-//                .disable()
-//            .formLogin()
-//                .disable()
-//            .httpBasic()
-//                .disable()
-//            .exceptionHandling()
-//                .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
-//            .and()
-//            .oauth2Login();
-//
-//        http
-//            .addFilterBefore(customOncePerRequestFilter(), UsernamePasswordAuthenticationFilter.class)
-//            .addFilterBefore(steamAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-//        return http.build();
-//    }
-
-//    @Override
-//    protected void configure(HttpSecurity http) throws Exception {
-//        http
-//                .cors().disable()
-//                .csrf().disable()
-//                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-//                .and()
-//                .authorizeRequests()
-//                .antMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api/v1.0/**").permitAll()
-//                .anyRequest().authenticated()
-//                .and()
-//                .addFilterBefore(customOncePerRequestFilter(), UsernamePasswordAuthenticationFilter.class)
-//                .addFilterBefore(steamAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class); // 여기서 Steam 인증 필터 추가
-//    }
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .csrf()
+                    .disable()
                 .authorizeRequests()
-                .antMatchers("/swagger", "/swagger-ui.html", "/swagger-ui/**", "/api/v1.0/**")
+                .antMatchers("/", "/login", "/swagger", "/v3/api-docs/**",  "/swagger-ui.html", "/swagger-ui/**", "/api/v1/**", "/api/v1/**/**", "/error")
                 .permitAll()
-                .anyRequest().authenticated()
+                .anyRequest()
+                .authenticated()
                 .and()
                 .openidLogin()
-                .defaultSuccessUrl("/login/success")
-//                .authenticationUserDetailsService(userDetailsService)
-//                .attributeExchange("http://specs.openid.net/auth/2.0")
-//                .attribute("openid.identity").type("http://schema.openid.net/namePerson")
-//                .and()
-//                .attribute("openid.steam_id").type("http://schema.steam.com/steam_id")
+                    .loginPage("/login")
+                    .permitAll()
                 .and()
-//                .and()
-//                .and()
+                .exceptionHandling()
+                    .authenticationEntryPoint(new CustomAuthenticationEntryPoint())
+                .and()
                 .logout()
                 .permitAll();
     }
-//    @Bean
-//    public SteamAuthenticationFilter steamAuthenticationFilter() throws Exception {
-//        SteamAuthenticationFilter filter = new SteamAuthenticationFilter(new AntPathRequestMatcher("/login/steam", "POST"));
-//        filter.setAuthenticationManager(authenticationManagerBean());
-//        return filter;
-//    }
 
-//    @Override
-//    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-//        auth.authenticationProvider(steamAuthenticationProvider); // Steam 인증 공급자 추가
-//    }
+
 }
