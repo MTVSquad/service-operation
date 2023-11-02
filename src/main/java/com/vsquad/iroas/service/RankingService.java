8a0acecb8b3d0074fb3b4b011344501128eeb09e
박성준 <myrhymetree@gmail.com>
2023-11-02 09:40:52 +0900

    Refactor : swagger 응답 예시 개선, 테스트 코드 변경

--- src/main/java/com/vsquad/iroas/service/RankingService.java
+++ src/main/java/com/vsquad/iroas/service/RankingService.java
@@ -1,8 +1,7 @@
 package com.vsquad.iroas.service;
 
-import com.vsquad.iroas.aggregate.dto.RankingDto;
-import com.vsquad.iroas.aggregate.dto.ReqRankingDto;
-import com.vsquad.iroas.aggregate.dto.ResRankingDto;
+import com.vsquad.iroas.aggregate.dto.request.ReqRankingDto;
+import com.vsquad.iroas.aggregate.dto.response.ResRankingDto;
 import com.vsquad.iroas.aggregate.entity.Player;
 import com.vsquad.iroas.aggregate.entity.Ranking;
 import com.vsquad.iroas.aggregate.vo.PlayTime;
