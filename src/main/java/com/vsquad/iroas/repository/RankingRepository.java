2bbf9bfaabed7359886b696ae382892d22a0d97a
박성준 <myrhymetree@gmail.com>
2023-11-02 23:40:52 +0900

    Chore : 유실된 파일 복구 및 충돌 해결

--- src/main/java/com/vsquad/iroas/repository/RankingRepository.java
+++ src/main/java/com/vsquad/iroas/repository/RankingRepository.java
@@ -1,6 +1,9 @@
 package com.vsquad.iroas.repository;
 
 import com.vsquad.iroas.aggregate.entity.Ranking;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,7 +13,8 @@ import java.util.Optional;
 @Repository
 public interface RankingRepository extends JpaRepository<Ranking, Long> {
 
-    List<Ranking> findByCreatorMapId(String creatorMapId);
+    @EntityGraph(attributePaths = {"player"}, type = EntityGraph.EntityGraphType.LOAD)
+    Page<Ranking> findByCreatorMapId(String creatorMapId, Pageable pageable);
 
     Optional<Ranking> findByPlayerIdAndCreatorMapId(Long playerId, String creatorMapId);
 }
